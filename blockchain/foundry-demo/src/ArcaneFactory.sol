cce21ff7a56579cd1475a31f9a4d2f8b3e4c7c89
faysalbadaoui <faysalbadaoui02@gmail.com>
2024-09-01 18:01:39 +0200

    fix inconsistency constructur amm instance

--- blockchain/foundry-demo/src/ArcaneFactory.sol
+++ blockchain/foundry-demo/src/ArcaneFactory.sol
@@ -7,11 +7,15 @@ import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract AMMFactory is Pausable, Ownable {
     address[] public allPairs;
+    address public _owner;
+
     mapping(address => mapping(address => address)) public getPair;
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
     
-    constructor(address initialOwner) Ownable(initialOwner) {}
+    constructor(address initialOwner) Ownable(initialOwner) {
+        _owner = initialOwner;
+    }
     
     function createPair(address token0, address token1, string memory lpTokenName, string memory lpTokenSymbol) public onlyOwner returns (address pair) {
         require(token0 != token1, "Identical addresses");
@@ -19,7 +23,7 @@ contract AMMFactory is Pausable, Ownable {
         require(getPair[token0][token1] == address(0), "Pair already exists");
 
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        ArcaneAMM newPair = new ArcaneAMM{salt: salt}(token0, token1, lpTokenName, lpTokenSymbol);
+        ArcaneAMM newPair = new ArcaneAMM{salt: salt}(_owner, token0, token1, lpTokenName, lpTokenSymbol);
 
         getPair[token0][token1] = address(newPair);
         getPair[token1][token0] = address(newPair);
